e36a7480e47dfbd014658b7bd0c23ceac959bb43
kaimov <mike@magic.org>
2023-03-16 12:09:21 +0600

    add navigation folder

--- NoCap.WebApi/Managers/Status.cs
+++
@@ -1,7 +0,0 @@
-namespace NoCap.Managers;
-
-public class Status
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
--- NoCap.WebApi/Request/CheckCodeResult.cs
+++
@@ -1,9 +0,0 @@
-namespace NoCap.Request
-
-{
-    public class CheckCodeResult
-    {
-        public bool Success { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
